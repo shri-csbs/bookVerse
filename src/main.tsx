131d463c2cee62066978a6b6425998fe6d085c62
shri-csbs <shrivarshinee.k.2024.csbs@rajalakshmi.edu.in>
2026-07-10 14:26:55 +0530

    change

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({
-  immediate: true,
-});
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
