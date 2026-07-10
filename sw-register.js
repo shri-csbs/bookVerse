5892891e50d5fa7162a684491f32892e79bcd789
shri-csbs <shrivarshinee.k.2024.csbs@rajalakshmi.edu.in>
2026-07-10 13:52:31 +0530

    Add service worker registration script

---
+++ sw-register.js
@@ -0,0 +1,15 @@
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker.register("service-worker.js")
+
+        .then(() => {
+
+            console.log("Service Worker Registered");
+
+        });
+
+    });
+
+}
