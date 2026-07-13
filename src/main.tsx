a2f9cb19001bf7e6dd9cb0bf2903d2123d6b296d
shri-csbs <shrivarshinee.k.2024.csbs@rajalakshmi.edu.in>
2026-07-13 19:31:36 +0530

    Refactor imports and remove service worker registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import { registerSW } from 'virtual:pwa-register';
-
-registerSW({
-  immediate: true,
-});
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
